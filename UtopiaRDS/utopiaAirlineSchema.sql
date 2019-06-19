1bf0d028717d6a07ea34c6ffab0252e12b938c99
Jonathan Lovelace <kingjon3377@gmail.com>
2019-06-19 17:02:34 -0400

    In airports table, expand name length to 75 characters.
    In the dataset I found, quite a few airports have names over 45 characters, and
    some are even over 60 characters.

--- UtopiaRDS/utopiaAirlineSchema.sql
+++ UtopiaRDS/utopiaAirlineSchema.sql
@@ -2,7 +2,7 @@ CREATE DATABASE IF NOT EXISTS `utopia`;
 USE `utopia`;
 CREATE TABLE `tbl_airports` (
   `code` char(4) NOT NULL,
-  `name` varchar(45) NOT NULL,
+  `name` varchar(75) NOT NULL,
   PRIMARY KEY (`code`),
   UNIQUE KEY `idtbl_airports_UNIQUE` (`code`)
 );
